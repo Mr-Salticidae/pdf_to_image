02e6456b1a5b7abc2513a6a28789fd00b29866b8
Charles <wc1225827513@gmail.com>
2020-11-15 19:36:08 +0800

    Add a check for target folder. Update the error message for the wrong path of the PDF file to make it clearer.
    If the target folder doesn't exist, print the error message to guide the user.

--- pdf_to_image.py
+++ pdf_to_image.py
@@ -15,18 +15,21 @@ except:
     target_folder_path = current_folder_path
 
 if os.path.exists(pdf_file_path):
-    file_name = re.search(r"/(\w+)\.pdf", pdf_file_path).group(1)
-    if target_format.lower() == 'png':
-        pages = convert_from_path(pdf_file_path)
-        for i in range(len(pages)):
-            image = pages[i]
-            image.save(os.path.join(target_folder_path, file_name + '.png'), 'PNG')
-    elif target_format == 'jpg' or target_format == 'JPEG':
-        pages = convert_from_path(pdf_file_path)
-        for i in range(len(pages)):
-            image = pages[i]
-            image.save(os.path.join(target_folder_path, file_name + '.jpg'), 'JPEG')
+    if os.path.exists(target_folder_path):
+        file_name = re.search(r"/(\w+)\.pdf", pdf_file_path).group(1)
+        if target_format.lower() == 'png':
+            pages = convert_from_path(pdf_file_path)
+            for i in range(len(pages)):
+                image = pages[i]
+                image.save(os.path.join(target_folder_path, file_name + '.png'), 'PNG')
+        elif target_format == 'jpg' or target_format == 'JPEG':
+            pages = convert_from_path(pdf_file_path)
+            for i in range(len(pages)):
+                image = pages[i]
+                image.save(os.path.join(target_folder_path, file_name + '.jpg'), 'JPEG')
+        else:
+            print('The first argument shoule be (png | PNG | jpg | JPEG), please try again.')
     else:
-        print('The first argument shoule be (png | PNG | jpg | JPEG), please try again.')
+        print('Couldn\'t find the target folder, please check the path of the target folder.')
 else:
-    print('Couldn\'t find the target PDF file, please check the file name.')
+    print('Couldn\'t find the target PDF file, please check the path of the PDF file.')
